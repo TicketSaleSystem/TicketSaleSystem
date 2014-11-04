654f33b79adbe1f348122e923a70d00cd9fafa8a
Solan Lbrahim <wangyx9169@gmail.com>
2014-11-05 01:01:08 +0800

    DLL添加

---
+++ code/TicketSaleSystem/TicketSaleSystem/ToolsHelper.cs
@@ -0,0 +1,51 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraTab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TicketSaleSystem.XTCZ;
+
+namespace TicketSaleSystem
+{
+    static class ToolsHelper
+    {
+        /// <summary>
+        /// 添加到Tab控件里
+        /// </summary>
+        /// <param name="Xuser">要添加到的TabControl</param>
+        /// <param name="Xuser">要添加的用户控件实例</param>
+        /// <param name="name"> 控件唯一的 name 属性</param>
+        /// <param name="caption">显示标题 caption</param>
+        public static void AddUserControl(XtraTabControl Xtab, Object obj, string name, string caption)
+        {
+            try
+            {
+                foreach (XtraTabPage Xpage in Xtab.TabPages)
+                {
+                    if (Xpage.Name == name)
+                    {
+                        Xtab.SelectedTabPage = Xpage;  // 存在则显示该页
+                        return;
+                    }
+                }
+                XtraTabPage page = new XtraTabPage();
+                page.Name = name;   //控件标示
+                page.Text = caption;  //显示标题
+                XtraUserControl Xuser = new XtraUserControl();
+                Xuser.Dock = DockStyle.Fill;  //dock属性 全屏撑大
+                XtraUserControl frm = (XtraUserControl)obj;
+                //TicketSaleSystem.TicketOperate.TicketOperate frm = new TicketSaleSystem.TicketOperate.TicketOperate();
+                frm.Show();
+                page.Controls.Add(frm);
+                Xtab.TabPages.Add(page);
+                Xtab.SelectedTabPage = page;  // 切换显示到新增页
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
